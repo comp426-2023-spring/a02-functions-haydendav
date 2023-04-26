b08e9750566a07c23a99ee2d123963de4b15544a
Hayden Davis <davishayden99@gmail.com>
2023-04-26 10:39:14 -0400

    5

--- cli.js
+++ cli.js
@@ -1,54 +1,79 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import fetch from 'node-fetch';
 import moment from 'moment-timezone';
+import fetch from 'node-fetch';
 
-const args = minimist(process.argv.slice(2));
-const timezone = moment.tz.guess();
+var argv = minimist(process.argv.slice(2))
 
-if (args.h) {
-  console.log(`
-Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-    -h            Show this help message and exit.
-    -n, -s        Latitude: N positive; S negative.
-    -e, -w        Longitude: E positive; W negative.
-    -z            Time zone: uses tz.guess() from moment-timezone by default.
-    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.
-  `);
-  process.exit(0);
+if (argv.h) {
+    console.log(
+    "\nUsage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n\t" +
+        "-h            Show this help message and exit.\n\t" +
+        "-n, -s        Latitude: N positive; S negative.\n\t" +
+        "-e, -w        Longitude: E positive; W negative.\n\t" +
+        "-z            Time zone: uses tz.guess() from moment-timezone by default.\n\t" +
+        "-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n\t" +
+        "-j            Echo pretty JSON from open-meteo API and exit.\n"
+    )
+    process.exit(0);
 }
 
-const latitude = (args.n || args.s) * (args.n ? 1 : -1);
-const longitude = (args.e || args.w) * (args.e ? 1 : -1);
-const timeZone = args.z || timezone;
-const days = args.d === undefined ? 1 : args.d;
-const jsonOutput = args.j;
+if ("t" in argv) {
+    var timezone = argv["t"]
+} else {
+    var timezone = moment.tz.guess()
+}
 
-const requestUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&timezone=${timeZone}`;
+if ("s" in argv) {
+    var lat = -1 * Math.round(argv["s"] * 100) / 100
+} else {
+    var lat = Math.round(argv["n"] * 100) / 100
+}
 
-async function getWeatherData() {
-  try {
-    const response = await fetch(requestUrl);
-    const data = await response.json();
+if ("e" in argv) {
+    var long = Math.round(argv["e"] * 100) / 100
+} else {
+    var long = -1 * Math.round(argv["w"] * 100) / 100
+}
 
-    if (jsonOutput) {
-      console.log(JSON.stringify(data, null, 2));
-      return;
-    }
-
-    if (days === 0) {
-      console.log('today');
-    } else if (days === 1) {
-      console.log('tomorrow');
+if ("d" in argv) {
+    if (0 <= argv["d"] && argv["d"] <= 6) {
+        var day = argv["d"]
     } else {
-      console.log(`in ${days} days`);
+        console.error("Error: '-d' arg must be between 0-6")
+        process.exit(0);
     }
-  } catch (error) {
-    console.error('Error:', error.message);
-    process.exit(1);
-  }
+} else {
+    var day = 1
 }
 
-getWeatherData();
+var url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + long + "&daily=precipitation_hours&timezone=" + timezone
+
+const response = await fetch(url);
+
+const data = await response.json();
+
+if ("j" in argv) {
+    console.log(data);
+    process.exit(0);
+}
+
+var weather = ""
+
+if (data.daily.precipitation_hours[day] > 0) {
+    weather += "You will need your galoshes "
+} else {
+    weather += "You will not need your galoshes "
+}
+
+if (day == 0) {
+    weather += "today."
+} else if (day > 1) {
+    weather += "in " + day + " days."
+} else {
+    weather += "tomorrow."
+}
+
+console.log(weather)
+process.exit(0);
